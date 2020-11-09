c5b77461098b0b4f90e249ee1473162975d63ad3
Frank Carr <gundamtrooperx@gmail.com>
2020-11-09 01:22:38 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import AboutMe from './pages/AboutMe';
-
 import './App.css';
 import Portfolio from './pages/Portfolio';
 
