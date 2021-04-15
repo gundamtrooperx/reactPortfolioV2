1887751b33331a26d4a98f327fa739e307b9bad7
Frank Carr <gundamtrooperx@gmail.com>
2021-04-14 17:26:27 -0700

    changed out the example description in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ function Project(props) {
   <img src={ props.image} className="card-img-top" alt={props.name}></img>
   <div className="card-body">
     <h5 className="card-title">{props.name}</h5>
-    <p className="card-text">{props.description}Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">{props.description}</p>
     <a href={props.github} className="btn btn-primary">Github</a>
   </div>
 </div> 
